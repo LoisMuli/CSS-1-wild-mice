36c34a7932e0e86c7d45264dfaf17d7dfc36f829
LoisMuli <122018285+LoisMuli@users.noreply.github.com>
2023-01-05 19:25:31 +0800

    Update wgs_4_hard_filtration.sh

--- wgs_4_hard_filtration.sh
+++ wgs_4_hard_filtration.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/bash
 
-#C57BL6J WGS
-
 # Hard Filtration
 
-##shell执行参数
-input=$1
-name=$2 #输出文件的名称
-outdir=$3 #输出文件的路径
+## parameters
+input=$1 # input file
+name=$2 # output name
+outdir=$3 # output path
 
 # Select SNP
 
@@ -27,6 +25,3 @@ gatk VariantFiltration \
 --filter-expression "MQRankSum < -12.500" --filter-name "MQRankSum-12.5" \
 --filter-expression "ReadPosRankSum < -8.000" --filter-name "ReadPosRankSum-8.0" \
 -O $3/${name}.filter.snp.vcf
-
-# delete filtered snp
-
