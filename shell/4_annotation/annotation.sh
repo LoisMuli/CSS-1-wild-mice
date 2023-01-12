9134bd7b9681c930c976eb12fff6b2e05a8a5b99
LoisMuli <122018285+LoisMuli@users.noreply.github.com>
2023-01-12 17:27:49 +0800

    Update annotation.sh

--- shell/4_annotation/annotation.sh
+++ shell/4_annotation/annotation.sh
@@ -8,8 +8,8 @@ outname=$2	# outname: YP,BLD...
 conda activate wgs
 
 # use VEP to annotate
-cd $1/$2/gatk
-vep --biotype --buffer_size 500 --check_existing --distance 5000 --numbers --regulatory --sift b --species mus_musculus --symbol --transcript_version --tsl --cache --input_file ${outname}.filter.vcf.gz --output_file $2
+cd $1/$2/anno
+vep --biotype --buffer_size 500 --check_existing --distance 5000 --numbers --regulatory --sift b --species mus_musculus --symbol --transcript_version --tsl --cache --input_file $1/$2/gatk/${outname}.pass.vcf.gz --output_file $2
 
 # quit conda environment
 conda deactivate
